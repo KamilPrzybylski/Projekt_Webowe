c4d5f77637e09042e93a7a57e3e8773f99d92d8d
wweiss <wojciech.weiss@uczen.zsk.poznan.pl>
2024-05-14 13:57:10 +0200

    adding aboutPage not final version

---
+++ src/pages/aboutPage/aboutPageContent/AboutPageContainer.style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  background-color: #333;
+`;
+
+export const Title = styled.h1`
+  color: #ff0000;
+`;
+
+export const Content = styled.p`
+  max-width: 800px;
+  margin: 20px;
+`;
