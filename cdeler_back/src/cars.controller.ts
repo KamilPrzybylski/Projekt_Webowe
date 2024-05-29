6dc720b1d2994d011d70585e1e660f61b9a037b3
Kamil Przybylski <macos@MI9-TelefonMI.macopedia>
2024-05-29 11:18:49 +0200

    commit

--- cdeler_back/src/cars.controller.ts
+++ cdeler_back/src/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Controller('cars')
@@ -8,5 +8,37 @@ export class CarsController {
   @Get()
   async getCars() {
     return this.prisma.cars.findMany();
+  }
+
+  @Get('/used')
+  async findUsed() {
+    return this.prisma.cars.findMany({
+      where: {
+        mileage: {
+          gt: 5
+        }
+      }
+    });
+  }
+
+  @Get('/new')
+  async findNew() {
+    return this.prisma.cars.findMany({
+      where: {
+        mileage: {
+          lte: 5
+        }
+      }
+    });
+  }
+
+  @Get(':id')
+  async findOne(@Param() params: any) {
+    const id = Number(params.id)
+    return this.prisma.cars.findUnique({
+      where: {
+        car_id: id
+      }
+    });
   }
 }
--- cdeler_front/src/components/Header/index.tsx
+++ cdeler_front/src/components/Header/index.tsx
@@ -1,10 +1,58 @@
+import { useEffect, useState } from 'react';
 import "./headerstyles.style.tsx"
 import logo from '../../png/logo1.png'
 import colorLogo from "../../png/logo-no-background.png";
 import {HeaderContainer, LogoContainer, MiniLogo, Nav, NavPackage, StyledLink} from "./headerstyles.style.tsx";
 import DropdownMenu from "../dropDownMenu/index.tsx";
 
+interface Car {
+    car_id: number,
+    brand: string,
+    model: string,
+    photo: string,
+    vin: string,
+    driveType: string,
+    mileage: number,
+    color: string,
+    description: string,
+    Engines_engine_id: number,
+    fuel_id: number,
+    categorie_id: number,
+    transaction_id: number,
+}
+
 const Header = () => {
+    const [cars, setCars] = useState<Car[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchCars = async () => {
+          try {
+            const response = await fetch('http://localhost:3000/cars');
+            if (!response.ok) {
+              throw new Error('Failed to fetch cars');
+            }
+            const data = await response.json();
+            setCars(data);
+          } catch (error) {
+            setError('Failed to fetch cars');
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchCars();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
     return (
         <HeaderContainer>
             <Nav>
@@ -21,6 +69,16 @@ const Header = () => {
             <LogoContainer title="logo" >
                 <img src={colorLogo} alt={"logo"}/>
             </LogoContainer>
+            <div>
+                <h1>Cars</h1>
+                <ul>
+                    {cars.map(car => (
+                    <li key={car.car_id}>
+                        {car.brand} {car.model} - {car.mileage} miles
+                    </li>
+                    ))}
+                </ul>
+            </div>
         </HeaderContainer>
 
     );
